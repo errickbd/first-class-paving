035b7fb1c8315914f5681d147a3cb59502e77498
Errick Basa-Denis <errickbd@gmail.com>
2023-12-31 22:52:22 -0600

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css";
-
+import "./Components/Navbar.css";
 
 
 function App() {
