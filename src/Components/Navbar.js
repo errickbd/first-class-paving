f859fd70a6732464a34d38d2e5e504f08a49ebed
Errick Basa-Denis <errickbd@gmail.com>
2023-12-31 21:14:12 -0600

    .

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
     return(
         <>
             <nav className='navbar'>
@@ -9,6 +13,36 @@ function Navbar() {
                     <Link to="/" className='navbar-logo'>
                         LOGO <i className="fab fa-typo3"></i>
                     </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'}/>
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                HOME
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                                ABOUT
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                                SERVICES
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/gallery' className='nav-links' onClick={closeMobileMenu}>
+                                GALLERY
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/quote' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                GET A QUOTE
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
 
