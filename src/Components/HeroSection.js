70cf702d12799bca8e8b54035ccce853446135b7
Errick Basa-Denis <errickbd@gmail.com>
2024-01-02 00:20:43 -0600

    .

--- src/Components/HeroSection.js
+++ src/Components/HeroSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import Logo from '../FCPLogo.jpg'
 
 
 function HeroSection() {
@@ -10,9 +11,10 @@ function HeroSection() {
             <video src='videos/construction01.mp4' autoPlay loop muted />
             <div className='hero-banner'>
                 <div className='hero-banner-left'>
-                    <p>Mon-Fri 8am-6pm | Sat 8am-12pm | Sun Appt. Only <br />
-                    Email: youremail@gmail.com <br />
-                    Phone: 123.456.7890
+                    <p><span>Hours:</span> <br />
+                    Mon-Fri 8am-6pm | Sat 8am-12pm | Sun Appt. Only <br />
+                    <span>Email:</span> youremail@gmail.com <br />
+                    <span>Phone:</span> 123.456.7890
                     </p>
                 </div>
                 
@@ -24,6 +26,27 @@ function HeroSection() {
                     </div> 
                 </div>
             </div>
+            <div className='home-section01'>
+               <img src={Logo} className='home-logo' />
+               <div className='home-info'>
+                    <div className='about-gist'>
+                        <h1><span>First Class Guarantee</span></h1>
+                        <p>We are a company based in the Chicagoland area, and we offer you some of the best work seen within the area. Give us a ring for repairs, sealcoating, crack filling, and grading. We work with residential, as well as, commercial clients.  </p>
+                    </div>
+                    <div className='services-gist'>
+                        <h1><span>What We Offer</span></h1>
+                        <p>We offer paving, concrete, crack fillings, grading. Contact for inquiries.  </p>
+                    </div>
+                    <div className='testimonials-gist'>
+                        <h1><span>Our Client Says</span></h1>
+                        <p>"Best company in the area. 10/10 Recommended" <br />
+                        -John Doe
+                        </p>
+                    </div>   
+               </div>
+                
+            </div>
+            
             
             
             
