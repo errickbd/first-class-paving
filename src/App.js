8c7f6d302f253d9db1d1b52b10144215c874ede8
Errick Basa-Denis <errickbd@gmail.com>
2024-01-02 10:19:38 -0600

    .

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./App.css";
 import "./Components/Navbar.css";
 import Home from './Components/pages/Home';
+import About from './Components/pages/About';
+import Services from './Components/pages/Services';
+import Gallery from './Components/pages/Gallery';
+import Quote from './Components/pages/Quote';
 
 
 function App() {
@@ -14,6 +18,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' exact element={<Home/>} />
+        <Route path='/about' exact element={<About/>} />
+        <Route path='/services' exact element={<Services/>} />
+        <Route path='/gallery' exact element={<Gallery/>} />
+        <Route path='/quote' exact element={<Quote/>} />
+        
       </Routes> 
     </Router>  
     </>
